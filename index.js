698d3e1969d66f5c96b0476e1419e1c4083cb46f
maerzhase <markus@lili16.de>
2023-07-20 12:10:03 +0200

    module-a: initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+function main() {
+	console.log("++++++++++++++++++++")
+	console.log("Calling Main")
+	console.log("Hello from module-a")
+	console.log("Thanks")
+	console.log("++++++++++++++++++++")
+}
+
+
+function helloWorld() {
+	console.log("=========")
+	console.log("=========")
+	console.log("=========")
+	console.log("hello world")
+	console.log("=========")
+	console.log("=========")
+	console.log("=========")
+}
+
+
+
+main()
+helloWorld()
